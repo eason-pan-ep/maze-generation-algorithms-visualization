4b4aeb6eb36e621769a4ec8a2766df15e06e0cdc
Eason Pan <eason.pan@proton.me>
2023-07-22 15:31:54 -0700

    dev and tested algorithm class initialization

---
+++ test.py
@@ -0,0 +1,13 @@
+from hunt_and_kill_algo import *
+
+
+def main():
+    test_grid_size = 3
+    test_body = HuntAndKillAlgorithm(test_grid_size)
+    
+    # test1 grid initialization
+    print(test_body.grid)
+    
+    
+if __name__ == "__main__":
+    main()
