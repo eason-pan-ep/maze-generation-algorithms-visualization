ef39a29d32ec66f5ee070af8cb2ee83be1b1d46f
Eason Pan <eason.pan@proton.me>
2023-07-22 22:32:22 -0700

    fixed bug in cell Class, implemented initialization sequence for visualization

--- cell.py
+++ cell.py
@@ -1,6 +1,8 @@
 import pygame
 
 class Cell:
+    
+    
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
@@ -11,27 +13,21 @@ class Cell:
             "right" : True,
         }
         self.is_visited = False
-        self.size = 5
+        self.border = 3
         
-    def draw_walls(self, screen:int, tile_size:int) -> None:
-        FILL_COLOUR = "#FFFFFF"
-        LINE_COLOUR = "#FFAC30"
-        BORDER_WIDTH = 2
+    def draw_walls(self, screen, tile_size) -> None:
+        FILL_COLOUR = "#525252"
+        LINE_COLOUR = "#FF9330"
         
-        draw_x = self.x * tile_size
-        draw_y = self.y * tile_size
-        
-        # if this cell is visited, fill it with white
-        if (self.is_visited):
-            #fill colour
-            pygame.draw.rect(screen, pygame.Color(FILL_COLOUR), (draw_x, draw_y, tile_size, tile_size))
-            if(self.walls["top"]):
-                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
-            if(self.walls["bottom"]):
-                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y + tile_size), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
-            if(self.walls["left"]):
-                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y), (draw_x, draw_y + tile_size), BORDER_WIDTH)
-            if(self.walls["right"]):
-                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x + tile_size, draw_y), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
+        x, y = self.x * tile_size, self.y * tile_size
+
+        if self.walls["top"]:
+            pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (x, y), (x + tile_size, y), self.border)
+        if self.walls["right"]:
+            pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (x + tile_size, y), (x + tile_size, y + tile_size), self.border)
+        if self.walls["bottom"]:
+            pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (x + tile_size, y + tile_size), (x , y + tile_size), self.border)
+        if self.walls["left"]:
+            pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (x, y + tile_size), (x, y), self.border)
             
             
--- hunt_and_kill_visualization.py
+++ hunt_and_kill_visualization.py
@@ -1,22 +1,25 @@
 import pygame
-from cell import *
 from random import choice
+from cell import *
+
+TILE_SIZE = 50
+COLUMN_COUNT = 10
+ROW_COUNT = 10
+
+
 
 def main():
-    TILE_SIZE = 50
-    COLUMN_COUNT = 10
-    ROW_COUNT = 10
-
-    # !! standard pygame framework start here !!
+    
+    # !! standard pygame framework !!
     pygame.init()
-    screen_width = COLUMN_COUNT * TILE_SIZE
-    scrren_height = ROW_COUNT * TILE_SIZE
+    screen_width = COLUMN_COUNT * TILE_SIZE + 4
+    scrren_height = ROW_COUNT * TILE_SIZE + 4
+
     screen = pygame.display.set_mode((screen_width, scrren_height))
     clock = pygame.time.Clock()
     
     grid = [[Cell(col, row) for row in range(ROW_COUNT)] for col in range(COLUMN_COUNT)]
     current_position = grid[0][0]
-    
     
 
     # pygame running mainloop
@@ -27,12 +30,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-                
+        
+        #initialize all cells
+        for row in range(ROW_COUNT):
+            for col in range(COLUMN_COUNT):
+                grid[row][col].draw_walls(screen, TILE_SIZE)
         
         pygame.display.flip()
         clock.tick(30)
     # !! standard pygame framework ends !!
     
-    
+
+
+
+
+
+
 if __name__ == "__main__":
     main()
