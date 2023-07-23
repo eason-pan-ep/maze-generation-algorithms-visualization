b618522d0afe2124890835b2154fbcb724a53462
Eason Pan <eason.pan@proton.me>
2023-07-22 21:46:11 -0700

    implemented draw cell method in Cell Class

--- cell.py
+++ cell.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Cell:
     def __init__(self, x, y) -> None:
         self.x = x
@@ -11,4 +13,25 @@ class Cell:
         self.is_visited = False
         self.size = 5
         
-    def draw_walls(self, )
+    def draw_walls(self, screen:int, tile_size:int) -> None:
+        FILL_COLOUR = "#FFFFFF"
+        LINE_COLOUR = "#FFAC30"
+        BORDER_WIDTH = 2
+        
+        draw_x = self.x * tile_size
+        draw_y = self.y * tile_size
+        
+        # if this cell is visited, fill it with white
+        if (self.is_visited):
+            #fill colour
+            pygame.draw.rect(screen, pygame.Color(FILL_COLOUR), (draw_x, draw_y, tile_size, tile_size))
+            if(self.walls["top"]):
+                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
+            if(self.walls["bottom"]):
+                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y + tile_size), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
+            if(self.walls["left"]):
+                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x, draw_y), (draw_x, draw_y + tile_size), BORDER_WIDTH)
+            if(self.walls["right"]):
+                pygame.draw.line(screen, pygame.Color(LINE_COLOUR), (draw_x + tile_size, draw_y), (draw_x + tile_size, draw_y + tile_size), BORDER_WIDTH)
+            
+            
--- visualization.py
+++ visualization.py
@@ -1,22 +1,38 @@
 import pygame
+from cell import *
 from random import choice
 
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 1000
-TILE = 50
+def main():
+    TILE_SIZE = 50
+    COLUMN_COUNT = 10
+    ROW_COUNT = 10
 
-pygame.init()
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    # !! standard pygame framework start here !!
+    pygame.init()
+    screen_width = COLUMN_COUNT * TILE_SIZE
+    scrren_height = ROW_COUNT * TILE_SIZE
+    screen = pygame.display.set_mode((screen_width, scrren_height))
+    clock = pygame.time.Clock()
+    
+    grid = [[Cell(col, row) for row in range(ROW_COUNT)] for col in range(COLUMN_COUNT)]
+    current_position = grid[0][0]
+    
+    
 
-while True:
-    screen.fill(pygame.Color("darkgray"))
+    # pygame running mainloop
+    while True:
+        screen.fill(pygame.Color("#525252"))
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+                
+        
+        pygame.display.flip()
+        clock.tick(30)
+    # !! standard pygame framework ends !!
     
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-            
-    
-    pygame.display.flip()
-    
-    
+if __name__ == "__main__":
+    main()
